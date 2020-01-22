fe296930838d9baf84f6ee1511a23d9643c14ea4
Jay Wen <jaywen89@gmail.com>
2020-01-22 15:41:40 -0500

    updated relationships

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,5 @@ class Owner < ApplicationRecord
     # has_secure_password
     has_many :plants
     has_many :plant_types, through: :plants
+    has_many :waterings
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,5 @@
 class Plant < ApplicationRecord
     belongs_to :plant_type
     belongs_to :owner
+    has_many :plant_waterings
 end
