f710715908b5971a9b20b3cd9a715d4c0c8cbdff
Jay Wen <jaywen89@gmail.com>
2020-01-23 10:23:23 -0500

    functioning search button

--- app/controllers/plant_types_controller.rb
+++ app/controllers/plant_types_controller.rb
@@ -2,6 +2,11 @@ class PlantTypesController < ApplicationController
 
     def index 
         @plant_types = PlantType.all
+        if params[:search]
+            @plant_types = PlantType.search(params[:search]).order("created_at DESC")
+        else
+            @plant_types = PlantType.all.order('created_at DESC')
+        end
     end 
 
     def show
